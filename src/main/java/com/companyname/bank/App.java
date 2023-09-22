394e5436e470284b3500e4cfb51affc8ee21145e
Sankar147 <136622137+Sankar147@users.noreply.github.com>
2023-09-22 12:02:06 +0530

    github hook trigger

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! modf in local" );
+        System.out.println( "Hello World! github hook trigger" );
     }
 }
